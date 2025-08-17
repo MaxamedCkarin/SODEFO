c544bc2737d31126fde46351b10f1a79d9d11320
MaxamedCkarin <maxmuudmaxamed79@gmail.com>
2025-08-17 17:52:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  toggleBtn.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
+}
+toggleBtn.addEventListener('click', () => {
+  const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+  toggleBtn.textContent = theme === 'dark' ? '☀️' : '🌙';
+});
