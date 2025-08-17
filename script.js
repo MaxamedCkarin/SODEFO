e6493fda835785ca64d01bea93b6fc917b461eee
MaxamedCkarin <maxmuudmaxamed79@gmail.com>
2025-08-17 19:11:35 +0300

    make header responsive and fix others

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const toggleBtn = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-  toggleBtn.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
-}
-toggleBtn.addEventListener('click', () => {
-  const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', theme);
-  localStorage.setItem('theme', theme);
-  toggleBtn.textContent = theme === 'dark' ? '☀️' : '🌙';
+// const toggleBtn = document.getElementById('theme-toggle');
+// const currentTheme = localStorage.getItem('theme');
+// if (currentTheme) {
+//   document.documentElement.setAttribute('data-theme', currentTheme);
+//   toggleBtn.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
+// }
+// toggleBtn.addEventListener('click', () => {
+//   const theme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+//   document.documentElement.setAttribute('data-theme', theme);
+//   localStorage.setItem('theme', theme);
+//   toggleBtn.textContent = theme === 'dark' ? '☀️' : '🌙';
+// });
+
+const toggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('.nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('show');
 });
